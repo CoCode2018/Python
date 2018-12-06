b5d0943a18bd9a3b7cca940116cba6b95ad14164
CoCode2018 <3221910341@qq.com>
2018-12-06 11:34:29 +0800

    2018-12-06

---
+++ 老男孩全栈14期/Day01/Exercise01.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+
+# 输出
+print("Hello World")
+
+# 注释
+    #  #号,单行注释
+    #  三引号,多行注释
+    
+# 变量
+print(1 + 3)
+print((1 + 3) * 2)
+print((1 + 3) * 2 - 6)
+print(((1 + 3) * 2 - 6) * 3)
+
+a = 1 + 3
+print(a)
+
+b = a * 2
+print(b)
+
+c = b - 6
+print(c)
+
+d = c * 3
+print(d)
